bd62b04e4ba8184cc64e4a36a5593a1fa3be5931
MattiKloepper <matthias@familie-kloepper.org>
2020-03-29 04:09:54 +0200

    Initialized DirectX test-project and cleaned up some configuration issues. Project can be run at least now, but just shows the default blue MonoGame screen.

---
+++ Volumetric Terrain Monogame Testbed.WinDX/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Volumetric_Terrain_Monogame_Testbed.WinDX
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
