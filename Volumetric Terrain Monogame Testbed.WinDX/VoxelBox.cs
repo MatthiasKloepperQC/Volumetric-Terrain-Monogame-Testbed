a6d7f42dc2a3ec0b9bfd05cea978a6bbd06e350d
MattiKloepper <matthias@familie-kloepper.org>
2020-04-10 01:47:00 +0200

    Introduced VoxelBox class.

---
+++ Volumetric Terrain Monogame Testbed.WinDX/VoxelBox.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
+{
+    /// <summary>
+    /// An Axis-Aligned-Bounding-Box that encompasses 256x256x256 voxels from the terrain density field.
+    /// </summary>
+    class VoxelBox
+    {
+    }
+}
