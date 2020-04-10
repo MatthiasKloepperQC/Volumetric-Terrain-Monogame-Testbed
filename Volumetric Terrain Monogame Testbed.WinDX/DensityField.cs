754d482958d16203e9d3ea1ef20b586c6d350284
MattiKloepper <matthias@familie-kloepper.org>
2020-04-10 02:23:19 +0200

    Introduced dimension member.

--- Volumetric Terrain Monogame Testbed.WinDX/DensityField.cs
+++ Volumetric Terrain Monogame Testbed.WinDX/DensityField.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
 {
+    /// <summary>
+    /// A three dimensional density field.
+    /// </summary>
     class DensityField
     {
+        #region Members
+        private (int x, int y, int z) dimension;
+        #endregion
+
+        #region Constructor
+        private DensityField()
+        {
+            this.dimension = (256, 256, 256);
+        }
+        #endregion
     }
 }
