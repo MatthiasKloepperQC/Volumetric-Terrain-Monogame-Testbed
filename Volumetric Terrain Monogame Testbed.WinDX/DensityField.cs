3bb9995e688c25a43b328c1c39398fbe09f8d373
MattiKloepper <matthias@familie-kloepper.org>
2020-04-13 01:11:04 +0200

    Added constructor to define the DensityField's dimension. Added constant for the default dimension for the parameterless constructor.

--- Volumetric Terrain Monogame Testbed.WinDX/DensityField.cs
+++ Volumetric Terrain Monogame Testbed.WinDX/DensityField.cs
@@ -5,19 +5,43 @@ using System.Text;
 namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
 {
     /// <summary>
-    /// A three dimensional density field.
+    /// A three dimensional density field with the discrete sample points layed out in a regular grid.
     /// </summary>
     class DensityField
     {
+        #region Contants
+        /// <summary>
+        /// The default dimension for each axis of the <see cref="DensityField"/>.
+        /// </summary>
+        private const int DefaultDimension=256;
+        #endregion
+
         #region Members
-        private (int x, int y, int z) dimension;
+        /// <summary>
+        /// The number of discrete sample points for each axis of the <see cref="DensityField"/>.
+        /// </summary>
+        private readonly (int x, int y, int z) dimension;
         #endregion
 
         #region Constructor
-        private DensityField()
+        /// <summary>
+        /// Creates a new <see cref="DensityField"/> instance using the <see cref="DefaultDimension"/> for each axis.
+        /// </summary>
+        private DensityField() : this(DensityField.DefaultDimension) { }
+
+        private DensityField(int dimension)
         {
+            // Check parameters.
+            if (dimension < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(dimension), "Each dimension of the " + nameof(DensityField) + " has to be greater than zero.");
+            }
+
             this.dimension = (256, 256, 256);
         }
+        #endregion
+
+        #region Properties
         #endregion
     }
 }
