43f2849a3b592a413df565ca2df0f64db6b16fb7
MattiKloepper <matthias@familie-kloepper.org>
2020-04-23 01:20:35 +0200

    Integrated basic code to set up camera, effect and geometry to paint a simple, coloured square to the screen.

--- Volumetric Terrain Monogame Testbed.WinDX/Game1.cs
+++ Volumetric Terrain Monogame Testbed.WinDX/Game1.cs
@@ -7,7 +7,10 @@ namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
 {
     public class Game1 : Game
     {
+        private FreeCamera camera;
         private readonly GraphicsDeviceManager graphics;
+        private VertexBuffer vbuffer;
+        private VolumeRaycastingEffect vrce;
 
         public Game1()
         {
@@ -22,13 +25,13 @@ namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            VolumeRaycastingEffect vrce = new VolumeRaycastingEffect(this.GraphicsDevice);
+            this.vrce = new VolumeRaycastingEffect(this.GraphicsDevice);
 
             // max Size for Texture2D on this card: 16.384 x 16.384 * 32 Bit
             // max Size for Texture3D on this card: 512 * 512 * 512 * 32 Bit
 
             // Initialize a basic camera (Pos: (0.0, 0.0, 0.0); Viewing direction (0.0, 0.0, -1.0); Up direction (0.0, 1.0, 0.0)
-            FreeCamera camera = new FreeCamera();
+            this.camera = new FreeCamera(this.GraphicsDevice);
 
             base.Initialize();
         }
@@ -53,6 +56,25 @@ namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
 
             // TODO: Add your update logic here
 
+            this.vrce.UseVertexColors = true;
+            this.vrce.WorldMatrix = Matrix.Identity;
+            this.vrce.ProjectionMatrix = this.camera.ProjectionMatrix;
+            this.vrce.ViewMatrix = this.camera.ViewMatrix;
+
+            this.vbuffer = new VertexBuffer(this.GraphicsDevice, VertexPositionColor.VertexDeclaration, 6, BufferUsage.WriteOnly);
+            VertexPositionColor[] vdata = new VertexPositionColor[6];
+            vdata[0] = new VertexPositionColor(new Vector3(-2, 2, -5), Color.Red);
+            vdata[1] = new VertexPositionColor(new Vector3(2, 2, -5), Color.Green);
+            vdata[2] = new VertexPositionColor(new Vector3(-2, -2, -5), Color.Blue);
+            vdata[3] = new VertexPositionColor(new Vector3(-2, -2, -5), Color.Yellow);
+            vdata[4] = new VertexPositionColor(new Vector3(2, 2, -5), Color.Orange);
+            vdata[5] = new VertexPositionColor(new Vector3(2, -2, -5), Color.Purple);
+            this.vbuffer.SetData(vdata);
+            this.GraphicsDevice.SetVertexBuffer(this.vbuffer);
+            RasterizerState nrs = new RasterizerState();
+            nrs.CullMode = CullMode.None;
+            //this.GraphicsDevice.RasterizerState = nrs;
+
             base.Update(gameTime);
         }
 
@@ -61,6 +83,14 @@ namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
             this.GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            // Alle vorgesehenen Durchläufe des Effektes durchgehen.
+            for (int passIndex = 0; passIndex < this.vrce.CurrentTechnique.Passes.Count; passIndex++)
+            {
+                this.vrce.CurrentTechnique.Passes[passIndex].Apply();
+
+                this.GraphicsDevice.DrawPrimitives(PrimitiveType.TriangleList, 0, 2);
+            }
 
             base.Draw(gameTime);
         }
