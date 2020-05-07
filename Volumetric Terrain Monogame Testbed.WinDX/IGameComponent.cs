ad7374916bf3636997a8439d1845b024341c52cc
MatthiasKloepperQC <matthias.kloepper@quantco.com>
2020-05-08 01:38:07 +0200

    Introduced IGameComponent interface.

---
+++ Volumetric Terrain Monogame Testbed.WinDX/IGameComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolConsulting.MonoGame.Testbed.VolumetricTerrain
+{
+    /// <summary>
+    /// Necessary interface for components managed through a <see cref="ComponentManager"/> instance.
+    /// </summary>
+    internal interface IGameComponent
+    {
+        #region Properties
+        /// <summary>
+        /// Gets whether this instance should currently be used by other components.
+        /// </summary>
+        public bool Enabled { get; }
+        #endregion
+    }
+}
